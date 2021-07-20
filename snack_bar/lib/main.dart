a1a445189731780634db5fdae3cf50d144c60b5d
Anand1magar <anandmagar321@gmail.com>
2021-07-20 23:43:18 +0545

    code change in show dialog

--- snack_bar/lib/main.dart
+++ snack_bar/lib/main.dart
@@ -1,64 +1,108 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+    return GetMaterialApp(
+      //GetMaterialApp is provided by GetX libary. we use this to do root management
+      // in our app without using the context
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("snack bar"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            Center(child: Text("Snack bar demo using Getx")),
+            ElevatedButton(
+              onPressed: () {
+                Get.snackbar(
+                  "SnackBar title", "This will be the message of Snack Bar",
+                  //This titleText and messageText property will replace the previous title and message
+                  titleText: Text("Another snack bar",
+                      style: TextStyle(color: Colors.red)),
+                  messageText: Text(
+                    "This is the another message",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  snackPosition: SnackPosition.BOTTOM,
+                  backgroundColor: Colors.black,
+                  maxWidth: 200,
+                  animationDuration: Duration(milliseconds: 1200),
+                  backgroundGradient:
+                      LinearGradient(colors: [Colors.red, Colors.green]),
+
+                  //While using border color ensure that u are using border width otherwise error will generated
+                  borderColor: Colors.pink,
+                  borderWidth: 4,
+                  boxShadows: [
+                    BoxShadow(
+                      color: Colors.yellow,
+                      offset: Offset(
+                          10, 20), //x = left and right, y= top and buttom
+                      spreadRadius: 20,
+                      blurRadius: 8,
+                    )
+                  ],
+
+                  //Dissmiss the snakbar by draging on virtical or horizental
+
+                  isDismissible: true,
+                  dismissDirection: SnackDismissDirection.HORIZONTAL,
+
+                  //Animation in SnakBar
+                  forwardAnimationCurve: Curves.bounceIn,
+                  icon: Icon(Icons.send),
+
+                  // Left bar indicator
+                  leftBarIndicatorColor: Colors.white,
+                  mainButton: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Retry",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  onTap: (val) {
+                    print("Snack bar on tap");
+                  },
+
+                  // overlayBlur: 3,
+                  showProgressIndicator: true,
+                  progressIndicatorBackgroundColor: Colors.deepOrange,
+
+                  //It will track what is the status of the snack bar  that when thesnackbar is open and close
+                  snackbarStatus: (value) {
+                    print(value);
+                  },
+
+                  //Use text form field: This userInputForm will hide the above or previous property
+
+                  // userInputForm: Form(
+                  //   child: Row(
+                  //     children: [
+                  //       Expanded(
+                  //         child: TextField(),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                );
+              },
+              child: Text("Show snake bar"),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
