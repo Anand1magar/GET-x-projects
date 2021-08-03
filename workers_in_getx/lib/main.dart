3b176e4ec02fe37ba5a40674be02f9b746c7c4ed
Anand1magar <anandmagar321@gmail.com>
2021-08-03 16:54:42 +0545

    ading workers in getx

--- workers_in_getx/lib/main.dart
+++ workers_in_getx/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
               onPressed: () {
